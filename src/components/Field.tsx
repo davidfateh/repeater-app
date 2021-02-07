7815190e5764bb447dbabf29d1a3eea31f8eb3d9
David Fateh <davidfateh@gmail.com>
2021-02-07 16:56:29 -0500

    adding code comments for clarity

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -16,12 +16,16 @@ interface FieldProps {
     sdk: FieldExtensionSDK;
 }
 
+/** An Item which represents an list item of the repeater app */
 interface Item {
     id: string;
     key: string;
     value: string;
 }
 
+/** A simple utility function to create a 'blank' item
+ * @returns A blank `Item` with a uuid
+*/
 function createItem(): Item {
     return {
         id: uuid(),
@@ -30,13 +34,20 @@ function createItem(): Item {
     };
 }
 
+/** The Field component is the Repeater App which shows up 
+ * in the Contentful field.
+ * 
+ * The Field expects and uses a `Contentful JSON field`
+ */
 const Field = (props: FieldProps) => {
     const { valueName = 'Value' } = props.sdk.parameters.instance as any;
     const [items, setItems] = useState<Item[]>([]);
 
     useEffect(() => {
+        // This ensures our app has enough space to render
         props.sdk.window.startAutoResizer();
 
+        // Every time we change the value on the field, we update internal state
         props.sdk.field.onValueChanged((value: Item[]) => {
             if (Array.isArray(value)) {
                 setItems(value);
@@ -44,11 +55,15 @@ const Field = (props: FieldProps) => {
         });
     });
 
+    /** Adds another item to the list */
     const addNewItem = () => {
         props.sdk.field.setValue([...items, createItem()]);
     };
 
-    const updateItemName = (item: Item, property: 'key' | 'value') => (
+    /** Creates an `onChange` handler for an item based on its `property`
+     * @returns A function which takes an `onChange` event 
+    */
+    const createOnChangeHandler = (item: Item, property: 'key' | 'value') => (
         e: React.ChangeEvent<HTMLInputElement>
     ) => {
         const itemList = items.concat();
@@ -59,6 +74,7 @@ const Field = (props: FieldProps) => {
         props.sdk.field.setValue(itemList);
     };
 
+    /** Deletes an item from the list */
     const deleteItem = (item: Item) => {
         props.sdk.field.setValue(items.filter((i) => i.id !== item.id));
     };
@@ -75,7 +91,7 @@ const Field = (props: FieldProps) => {
                                     name="key"
                                     labelText="Item Name"
                                     value={item.key}
-                                    onChange={updateItemName(item, 'key')}
+                                    onChange={createOnChangeHandler(item, 'key')}
                                 />
                             </TableCell>
                             <TableCell>
@@ -84,7 +100,7 @@ const Field = (props: FieldProps) => {
                                     name="value"
                                     labelText={valueName}
                                     value={item.value}
-                                    onChange={updateItemName(item, 'value')}
+                                    onChange={createOnChangeHandler(item, 'value')}
                                 />
                             </TableCell>
                             <TableCell align="right">
